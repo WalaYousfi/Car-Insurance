7d72adbe809949bba87814480e73693663958ca8
Wala Yousfi <yousfiwala9@gmail.com>
2024-05-23 00:24:12 +0100

    try remove

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,36 +1,42 @@
 import { NgModule } from '@angular/core';
-import { BrowserModule, provideClientHydration } from '@angular/platform-browser';
+import {
+  BrowserModule,
+  provideClientHydration,
+} from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
-import {MatSidenavModule} from '@angular/material/sidenav';
+import { MatSidenavModule } from '@angular/material/sidenav';
 import { HomePageComponent } from './home-page/home-page.component';
 import { ShellComponent } from './shell/shell.component';
 import { MatListModule } from '@angular/material/list';
-import { TryComponent } from './try/try.component';
 
-import {FormBuilder, Validators, FormsModule, ReactiveFormsModule} from '@angular/forms';
-import {MatButtonModule} from '@angular/material/button';
-import {MatInputModule} from '@angular/material/input';
-import {MatFormFieldModule} from '@angular/material/form-field';
-import {MatStepperModule} from '@angular/material/stepper';
-import {MatSelectModule} from '@angular/material/select';
+import {
+  FormBuilder,
+  Validators,
+  FormsModule,
+  ReactiveFormsModule,
+} from '@angular/forms';
+import { MatButtonModule } from '@angular/material/button';
+import { MatInputModule } from '@angular/material/input';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatStepperModule } from '@angular/material/stepper';
+import { MatSelectModule } from '@angular/material/select';
 import { AllStepsComponent } from './all-steps/all-steps.component';
-import {MatToolbarModule} from '@angular/material/toolbar';
-import {MatMenuModule} from '@angular/material/menu';
-import {MatIconModule} from '@angular/material/icon';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatMenuModule } from '@angular/material/menu';
+import { MatIconModule } from '@angular/material/icon';
 import { FirstStepComponent } from './steps/first-step/first-step.component';
 import { SecondStepComponent } from './steps/second-step/second-step.component';
 import { LastStepComponent } from './steps/last-step/last-step.component';
 import { DetailComponent } from './detail/detail.component';
-import { NgOtpInputModule } from  'ng-otp-input';
+import { NgOtpInputModule } from 'ng-otp-input';
 @NgModule({
   declarations: [
     AppComponent,
     HomePageComponent,
     ShellComponent,
-    TryComponent,
     AllStepsComponent,
     FirstStepComponent,
     SecondStepComponent,
@@ -52,12 +58,9 @@ import { NgOtpInputModule } from  'ng-otp-input';
     MatToolbarModule,
     MatMenuModule,
     MatIconModule,
-    NgOtpInputModule
+    NgOtpInputModule,
   ],
-  providers: [
-    provideClientHydration(),
-    provideAnimationsAsync()
-  ],
-  bootstrap: [AppComponent]
+  providers: [provideClientHydration(), provideAnimationsAsync()],
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
